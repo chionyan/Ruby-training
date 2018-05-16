92caa1a45948ddcd04a86a47f4b04b48e3d205ed
chionyan <ariariarissa@gmail.com>
2018-05-16 14:31:17 +0900

    ・テストをメソッドごとにdescribeでくくる
    ・各処理に対する結果をitでくくる

--- spec/rgb_spec.rb
+++ spec/rgb_spec.rb
@@ -1,15 +1,28 @@
 require_relative '../chapter04/rgb'
 
 RSpec.describe 'Conversion RGB' do
-  it "test_to_hex" do
-    expect(to_hex(0, 0, 0)).to eq '#000000'
-    expect(to_hex(255, 255, 255)).to eq '#ffffff'
-    expect(to_hex(4, 60, 120)).to eq '#043c78'
-  end
+  describe '#to_hex' do
+    it "(0,0,0) -> #000000" do
+      expect(to_hex(0, 0, 0)).to eq '#000000'
+    end
+    it "(255, 255, 255 -> #ffffff" do
+      expect(to_hex(255, 255, 255)).to eq '#ffffff'
+    end
+    it "(4, 60, 120) -> #043c78" do
+      expect(to_hex(4, 60, 120)).to eq '#043c78'
+    end
+   end
 
-  it "test_to_ints" do
-    expect(to_ints('#000000')).to eq [0, 0, 0]
-    expect(to_ints('#ffffff')).to eq [255, 255, 255]
-    expect(to_ints('#043c78')).to eq [4, 60, 120]
+  describe "#to_ints" do
+    it "#000000 -> [0, 0, 0]" do
+      expect(to_ints('#000000')).to eq [0, 0, 0]
+    end
+    it "#ffffff -> [255, 255, 255]" do
+      expect(to_ints('#ffffff')).to eq [255, 255, 255]
+    end
+    it "#043c78 -> [4, 60, 120]" do
+      expect(to_ints('#043c78')).to eq [4, 60, 120]
+    end
   end
 end
+
