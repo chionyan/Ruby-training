57c500a6f7232866317f103e83cd6df7e1197a7a
chionyan <ariariarissa@gmail.com>
2018-05-17 12:30:59 +0900

    [fix]convert_lengthメソッドを実装
    - 仮実装から実際に動くメソッドを実装
    - 各単位におけるm換算の数値を変数に格納
    - temp_meterにbefore_lengthをメートルに変換した値を一時格納
    - after_lengthにtemp_meterを変換後の長さに変換した値を格納
    - after_lengthを小数第3位で四捨五入した値を返す

--- chapter05/convert_length.rb
+++ chapter05/convert_length.rb
@@ -1,3 +1,29 @@
 def convert_length (before_length, before_unit, after_unit)
-  39.37
+  meter_to_meter = 1.00
+  feet_to_meter = 3.28
+  inch_to_meter = 39.37
+
+  # 変換元の長さをメートルに変換
+  temp_meter =
+    case before_unit
+    when 'm'
+      before_length / meter_to_meter
+    when 'ft'
+      before_length / feet_to_meter
+    when 'in'
+      before_length / inch_to_meter
+    end
+
+  # メートルを変換後の長さに変換
+  after_length =
+    case after_unit
+    when 'm'
+      temp_meter * meter_to_meter 
+    when 'ft'
+      temp_meter * feet_to_meter 
+    when 'in'
+      temp_meter * inch_to_meter 
+    end
+
+  after_length.round(2)
 end
