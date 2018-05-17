ce6b43a07735ae5ddc286ce0f30ca60da4746eb3
chionyan <ariariarissa@gmail.com>
2018-05-17 12:38:21 +0900

    [fix]convert_lengthメソッドにドキュメンテーションコメント追加
    - convert_lengthメソッドに概要、引数、戻り値の説明を追加

--- chapter05/convert_length.rb
+++ chapter05/convert_length.rb
@@ -1,3 +1,9 @@
+# メートル(m)、フィート(ft)、インチ(in)の単位を相互に変換する
+# @param before_length[Integer] 変換元の長さ
+# @param before_unit[String] 変換元の単位
+# @param after_unit[String] 変換後の単位
+# @return [Float] 変換後の長さ、端数が出る場合は小数第3位で四捨五入する
+
 def convert_length (before_length, before_unit, after_unit)
   meter_to_meter = 1.00
   feet_to_meter = 3.28
