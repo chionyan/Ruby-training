7c683734eacb2b9a8aa28e5dbc6a3fcc8ccccf0e
chionyan <ariariarissa@gmail.com>
2018-05-17 15:16:35 +0900

    [fix]convert_lengthメソッドの第2、第3引数をキーワード引数に修正
    - convert_length_test.rb、convert_length_spec.rbの第2、第3引数をキーワード引数に修正
    - convert_length_spec.rbのit内のメッセージを修正
    - convert_length.rbのconvert_lengthメソッドの第2、第3引数をキーワード引数に修正
    - ついでに第1引数の変数名も修正
    - 上記に伴うメソッド内の修正
    - 上記に伴うドキュメンテーションコメントの修正

--- chapter05/convert_length.rb
+++ chapter05/convert_length.rb
@@ -1,15 +1,15 @@
 # メートル(m)、フィート(ft)、インチ(in)の単位を相互に変換する
-# @param before_length[Integer] 変換元の長さ
-# @param before_unit[String] 変換元の単位
-# @param after_unit[String] 変換後の単位
+# @param length[Integer] 変換元の長さ
+# @param from:[Symbol] 変換元の単位
+# @param to:[Symbol] 変換後の単位
 # @return [Float] 変換後の長さ、端数が出る場合は小数第3位で四捨五入する
 
-def convert_length (before_length, before_unit, after_unit)
+def convert_length (length, from: :m, to: :m)
   units = {
     m: 1.0,
     ft: 3.28,
     in: 39.37
   }
 
-  (before_length / units[before_unit] * units[after_unit]).round(2)
+  (length / units[from] * units[to]).round(2)
 end
