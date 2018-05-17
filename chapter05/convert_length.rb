6e521cd1101802bdf79f802ed7a39c48323c8c9c
chionyan <ariariarissa@gmail.com>
2018-05-17 14:54:43 +0900

    [fix]単位ハッシュの導入、計算式の修正
    - 各単位のメートル換算値を格納していた変数3つをハッシュにまとめた
    - ハッシュ導入によるcase文の削除
    - 返り値を求める計算式を一つにまとめる

--- chapter05/convert_length.rb
+++ chapter05/convert_length.rb
@@ -5,31 +5,11 @@
 # @return [Float] 変換後の長さ、端数が出る場合は小数第3位で四捨五入する
 
 def convert_length (before_length, before_unit, after_unit)
-  meter_to_meter = 1.00
-  feet_to_meter = 3.28
-  inch_to_meter = 39.37
+  units = {
+    'm' => 1.0,
+    'ft' => 3.28,
+    'in'=> 39.37
+  }
 
-  # 変換元の長さをメートルに変換
-  temp_meter =
-    case before_unit
-    when 'm'
-      before_length / meter_to_meter
-    when 'ft'
-      before_length / feet_to_meter
-    when 'in'
-      before_length / inch_to_meter
-    end
-
-  # メートルを変換後の長さに変換
-  after_length =
-    case after_unit
-    when 'm'
-      temp_meter * meter_to_meter 
-    when 'ft'
-      temp_meter * feet_to_meter 
-    when 'in'
-      temp_meter * inch_to_meter 
-    end
-
-  after_length.round(2)
+  (before_length / units[before_unit] * units[after_unit]).round(2)
 end
