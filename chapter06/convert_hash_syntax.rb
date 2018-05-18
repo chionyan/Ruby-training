0f58cf96e3dc6299426d2274a567652b22efea52
chionyan <ariariarissa@gmail.com>
2018-05-18 14:59:18 +0900

    [add]#convert_hash_syntax仮実装、RSpec作成
    - #convert_hash_syntaxをテストするRSpecを作成し、テストを実装
    - RSpecのテストがパスする#convert_hash_syntaxを仮実装

---
+++ chapter06/convert_hash_syntax.rb
@@ -0,0 +1,9 @@
+def convert_hash_syntax(syntax)
+  <<~TEXT
+  {
+    name: 'Alice',
+    age: 20,
+    gender: :female
+  }
+  TEXT
+end
---
+++ spec/convert_hash_syntax_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../chapter06/convert_hash_syntax'
+
+RSpec.describe 'Convert Hash Syntax' do
+  describe '#convert_hash_syntax' do
+    it '古い記法のHash文字列を変換すると、新しい記法のHash文字列になること' do
+      old_syntax = <<~TEXT
+      {
+        :name=> 'Alice',
+        :age=>20,
+        :gender => :female 
+      }
+      TEXT
+      new_syntax = <<~TEXT
+      {
+        name: 'Alice',
+        age: 20,
+        gender: :female
+      }
+      TEXT
+      expect(convert_hash_syntax(old_syntax)).to eq new_syntax
+    end
+  end
+end
