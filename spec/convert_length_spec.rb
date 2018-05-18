c41063920583c491c57c39754a26d8c1a6c4d8ed
chionyan <ariariarissa@gmail.com>
2018-05-18 12:44:31 +0900

    [fix]convert_length_spec.rbのit後の文字列を修正
    - it後の文字列を#convert_lengthの振る舞いを表すように修正

--- spec/convert_length_spec.rb
+++ spec/convert_length_spec.rb
@@ -2,15 +2,15 @@ require_relative '../chapter05/convert_length'
 
 RSpec.describe 'Conversion Length' do
   describe '#convert_length' do
-    it '(1, from: :m, to: :in) -> 39.37' do
+    it '1mをinに変換すると、39.37になること' do
       expect(convert_length(1, from: :m, to: :in)).to eq 39.37
     end
 
-    it '(15, from: :in, to: :m) -> 0.38' do
+    it '15inをmに変換すると、 0.38になること' do
       expect(convert_length(15, from: :in, to: :m)).to eq 0.38
     end
 
-    it '(35000, from: :ft, to: :m) -> 10670.73' do
+    it '35000ftをmに変換すると、 10670.73になること' do
       expect(convert_length(35000, from: :ft, to: :m)).to eq 10670.73
     end
   end
