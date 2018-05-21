64fcac2164ae1411356b37ede2fa0a72269c17a7
chionyan <ariariarissa@gmail.com>
2018-05-21 14:34:10 +0900

    [fix]#convert_hash_syntaxの本実装
    - #convert_hash_syntaxに、正規表現を使って=>を使った記法から、=>を使わずコロン(:)を右側に付ける記法に変換する処理を実装
    - #convert_hash_syntaxのYARDを追加

--- chapter06/convert_hash_syntax.rb
+++ chapter06/convert_hash_syntax.rb
@@ -1,9 +1,8 @@
+# =>を使った記法から、=>を使わずコロン(:)を右側に付ける記法に変換する
+# 変換後の:の後のスペースは1個に統一する
+# 
+# @param syntax [String] 変換元のハッシュ文字列
+# @return [String] 変換後のハッシュ文字列
 def convert_hash_syntax(syntax)
-  <<~TEXT
-  {
-    name: 'Alice',
-    age: 20,
-    gender: :female
-  }
-  TEXT
+  syntax.gsub(/:(?<key>\w+)\s*=>\s*/,'\k<key>: ')
 end
