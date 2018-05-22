c154fda6700ee6a0eacf0045915de9df6c519fd4
chionyan <ariariarissa@gmail.com>
2018-05-22 13:09:46 +0900

    [fix]Gateクラスの仮実装
    - Gate#initialize, #enter, #exitを仮実装
    - 仮実装が通過するように、specファイルを実装

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,9 +1,19 @@
 require_relative '../chapter07/gate'
+require_relative '../chapter07/ticket'
 
 RSpec.describe 'Gate' do
-  describe 'Gate#initialize' do
-    it 'Gateオブジェクトが作成できること' do
-      expect(Gate.new).to_not be_nil
+  umeda = Gate.new(:umeda)
+  juso = Gate.new(:juso)
+  
+  context '150円の切符を購入して、' do
+    ticket = Ticket.new(150)
+    
+    context '梅田で入場した場合、' do
+      umeda.enter(ticket)
+
+      it '十三で出場することができる' do
+        expect(juso.exit(ticket)).to eq true
+      end
     end
   end
 end
