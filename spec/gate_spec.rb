2c1b243027f0e5932d90558cada1c425799489a3
chionyan <ariariarissa@gmail.com>
2018-05-22 16:12:14 +0900

    [fix]specファイルに三つ目、四つ目のテスト実装
    - specファイルに三つ目、四つ目のテスト実装
    - let, beforeでリファクタリング

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,16 +1,16 @@
 require_relative '../chapter07/gate'
 require_relative '../chapter07/ticket'
 
-RSpec.describe 'Gate' do
-  umeda = Gate.new(:umeda)
-  juso = Gate.new(:juso)
-  mikuni = Gate.new(:mikuni)
-  
+RSpec.describe Gate do
+  let(:umeda) { Gate.new(:umeda) }
+  let(:juso) { Gate.new(:juso) }
+  let(:mikuni) { Gate.new(:mikuni) }
+
   context '150円の切符を購入して、' do
-    ticket = Ticket.new(150)
+    let(:ticket) { Ticket.new(150) }
     
     context '梅田で入場した場合、' do
-      umeda.enter(ticket)
+      before { umeda.enter(ticket) }
 
       it '十三で出場することができる' do
         expect(juso.exit(ticket)).to eq true
@@ -18,6 +18,26 @@ RSpec.describe 'Gate' do
 
       it '三国で出場することができない' do
         expect(mikuni.exit(ticket)).to eq false
+      end
+    end
+
+    context '十三で入場した場合、' do
+      before { juso.enter(ticket) }
+
+      it '三国で出場することができる' do
+        expect(mikuni.exit(ticket)).to eq true  
+      end
+    end
+  end
+
+  context '190円の切符を購入して、' do
+    let(:ticket) { Ticket.new(190) }
+
+    context '梅田で入場した場合、' do
+      before { umeda.enter(ticket) }
+
+      it '三国で出場することができる' do
+        expect(mikuni.exit(ticket)).to eq true
       end
     end
   end
