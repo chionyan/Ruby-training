cee669425a8f7775a9600e99cc878500e99cb9cd
chionyan <ariariarissa@gmail.com>
2018-05-24 14:35:09 +0900

    [feature]Bank, Team の実装, テストコードの変更
    - Bank, Team クラス実装
    - Bank, Team クラスに DeepFreezable モジュールをミックスイン
    - minitest, spec テストに3つのテストケースを実装

--- spec/chapter08/deep_freezable_spec.rb
+++ spec/chapter08/deep_freezable_spec.rb
@@ -1,7 +1,16 @@
-require './chapter08/deep_freezable'
+require './chapter08/bank'
+require './chapter08/team'
 
 RSpec.describe DeepFreezable do
-  it 'モジュールが参照できること' do
-    expect(!!DeepFreezable).to be true
+  it '配列の値が正しいこと' do
+    expect(['Japan', 'US', 'India']).to eq Team::COUNTRIES
+  end
+  
+  it '配列自身がfreezeされていること' do
+    expect(Team::COUNTRIES.frozen?).to be true
+  end
+
+  it '配列の要素が全てfreezeされていること' do
+    expect(Team::COUNTRIES.all?{ |country| country.frozen? } ).to be true
   end
 end
