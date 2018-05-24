f96119d1770518f521159af429036ae204b95a86
chionyan <ariariarissa@gmail.com>
2018-05-24 14:54:08 +0900

    [feature]Hash の場合のテストケース追加
    - spec ファイルは場合分けを context でまとめた
    - spec ファイルの説明文を微修正

--- chapter08/deep_freezable_test.rb
+++ chapter08/deep_freezable_test.rb
@@ -13,4 +13,18 @@ class DeepFreezableTest < Minitest::Test
     # 配列の要素が全て freeze されているか？
     assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
+
+  def test_deep_freeze_to_hash
+    # ハッシュの値は正しいか？
+    assert_equal(
+      {'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'},
+      Bank::CURRENCIES
+    )
+
+    # ハッシュ自身が freeze されているか？
+    assert Bank::CURRENCIES.frozen?
+
+    # ハッシュの要素（キーと値）が全て freeze されているか？
+    assert Bank::CURRENCIES.all?{ |key, value| key.frozen? && value.frozen? }
+  end
 end
--- spec/chapter08/deep_freezable_spec.rb
+++ spec/chapter08/deep_freezable_spec.rb
@@ -2,15 +2,35 @@ require './chapter08/bank'
 require './chapter08/team'
 
 RSpec.describe DeepFreezable do
-  it '配列の値が正しいこと' do
-    expect(['Japan', 'US', 'India']).to eq Team::COUNTRIES
-  end
-  
-  it '配列自身がfreezeされていること' do
-    expect(Team::COUNTRIES.frozen?).to be true
+  context '引数が配列の場合' do
+    it '配列の値が正しいこと' do
+      expect(['Japan', 'US', 'India']).to eq Team::COUNTRIES
+    end
+
+    it '配列自身が freeze されていること' do
+      expect(Team::COUNTRIES.frozen?).to be true
+    end
+
+    it '配列の要素が全て freeze されていること' do
+      expect(Team::COUNTRIES.all?{ |country| country.frozen? } ).to be true
+    end
   end
 
-  it '配列の要素が全てfreezeされていること' do
-    expect(Team::COUNTRIES.all?{ |country| country.frozen? } ).to be true
+  context '引数がハッシュの場合' do
+    it 'ハッシュの値が正しいこと' do
+      expect(
+        {'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'}
+      ).to eq Bank::CURRENCIES
+    end
+
+    it 'ハッシュ自身が freeze されていること' do
+      expect(Bank::CURRENCIES.frozen?).to be true
+    end
+
+    it 'ハッシュの要素（キーと値）が全て freeze されていること' do
+      expect(
+        Bank::CURRENCIES.all?{ |key, value| key.frozen? && value.frozen? }
+      ).to be true
+    end
   end
 end
