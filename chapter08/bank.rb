1bcbca824ffeadfa3bc15172d1f22441317272d1
chionyan <ariariarissa@gmail.com>
2018-05-24 14:51:13 +0900

    [fix]CURRENCIES の typo 修正
    - ハッシュが [] になっていたのを {} に修正

--- chapter08/bank.rb
+++ chapter08/bank.rb
@@ -3,5 +3,5 @@ require_relative './deep_freezable'
 class Bank
   extend DeepFreezable
 
-  CURRENCIES = deep_freeze(['Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'])
+  CURRENCIES = deep_freeze({'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'})
 end
