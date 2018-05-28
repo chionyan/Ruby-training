fed62a276723ca29df5b555934109a7d6c01a86c
chionyan <ariariarissa@gmail.com>
2018-05-28 18:05:55 +0900

    [modify]regexp_check のリファクタリング
    - spec テスト導入のため、クラス追加
    - 各処理におけるメソッドの切り出し

--- chapter09/regexp_checker.rb
+++ chapter09/regexp_checker.rb
@@ -1,18 +1,43 @@
-print 'Text?: '
-text = gets.chomp
+class RegexpChecker
+  def run
+    text = input_text
+    regexp = conv_regexp
+    output(text, regexp)
+  end
 
-begin
-  print 'Pattern?: '
-  pattern = gets.chomp
-  regexp = Regexp.new(pattern)
-rescue RegexpError => e
-  puts "Invalid pattern: #{e.message}"
-  retry
-end
+  private
 
-matches = text.scan(regexp)
-if matches.size > 0
-  puts "Matched: #{matches.join(',')}"
-else
-  puts "Nothing matched."
+  def input_text
+    print 'Text?: '
+    gets.chomp
+  end
+
+  def input_pattern
+    print 'Pattern?: '
+    gets.chomp
+  end
+
+  def conv_regexp
+    retry_count = 0
+    begin
+      Regexp.new(input_pattern)
+    rescue RegexpError => e
+      retry_count += 1
+      if retry_count <= 3
+        puts "Invalid pattern: #{e.message}"
+        retry
+      else
+        puts "Quit."
+      end
+    end
+  end
+
+  def output(text, regexp)
+    matches = text.scan(regexp)
+    if matches.size > 0
+      puts "Matched: #{matches.join(',')}"
+    else
+      puts "Nothing matched."
+    end
+  end
 end
