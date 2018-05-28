87cdbb92fb69a1e016c1556dfd719a2388d4a185
chionyan <ariariarissa@gmail.com>
2018-05-28 18:08:37 +0900

    [feature]regexp_checker_spec の実装
    - before ブロックで RegexpChecker#new を行う
    - 標準入力の RegexpChecker#input_text, RegexpChecker#input_pattern のスタブを作成
    - 3つのシナリオでのテストを作成

---
+++ spec/chapter09/regexp_checker_spec.rb
@@ -0,0 +1,33 @@
+require './chapter09/regexp_checker'
+
+RSpec.describe RegexpChecker do
+  before do
+    @regexp_checker = RegexpChecker.new
+    allow(@regexp_checker).to receive(:input_text).and_return(text)
+    allow(@regexp_checker).to receive(:input_pattern).and_return(pattern)
+  end
+
+  context 'テキストが正規表現にマッチする場合' do
+    let(:text){ '123-456-789' }
+    let(:pattern){ '[1-6]+' }
+    it '"Matched: "に続いて、全てのマッチした文字列が表示されること' do
+      expect{ @regexp_checker.run }.to output("Matched: 123,456\n").to_stdout
+    end
+  end
+
+  context 'テキストが正規表現にマッチしない場合' do
+    let(:text){ 'abc-def-hig' }
+    let(:pattern){ '[1-6]+' }
+    it '"Nothing matched."が出力されること' do
+      expect{ @regexp_checker.run }.to output("Nothing matched.\n").to_stdout
+    end
+  end
+
+  context '正規表現として無効な文字列が入力された場合' do
+    let(:text){ '123-456-789' }
+    let(:pattern){ '[1-6+' }
+    it 'エラーが発生すること' do
+      expect{ @regexp_checker.run }.to raise_error
+    end
+  end
+end
