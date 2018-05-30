9891611f071f5448a094fd3d08c1bfc038caa4d8
chionyan <ariariarissa@gmail.com>
2018-05-30 17:14:54 +0900

    [feature]Effects#echo の実装
    - effects_spec(test) にテストケースを記述
    - テストがパスすることの確認

--- spec/chapter10/effects_spec.rb
+++ spec/chapter10/effects_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe Effects do
       expect(effect.call('Ruby is fun!')).to eq 'ybuR si !nuf'
     end
   end
+
+  context 'エコーエフェクトをかけた場合' do
+    let(:effect){ Effects.echo(rate) }
+
+    context '2回かけた場合' do
+      let(:rate){ 2 }
+
+      it '2回ずつ各文字を繰り返すこと' do
+        expect(effect.call('Ruby is fun!')).to eq 'RRuubbyy iiss ffuunn!!'
+      end
+    end
+
+    context '3回かけた場合' do
+      let(:rate){ 3 }
+
+      it '3回ずつ各文字を繰り返すこと' do
+        expect(effect.call('Ruby is fun!')).to eq 'RRRuuubbbyyy iiisss fffuuunnn!!!'
+      end
+    end
+  end
 end
