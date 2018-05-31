83d77a5f284c866ccf84e6521f3cb0a25a4b2d41
chionyan <ariariarissa@gmail.com>
2018-05-31 10:31:06 +0900

    [docs]YARD コメントの追加

--- chapter10/lib/effects.rb
+++ chapter10/lib/effects.rb
@@ -1,16 +1,28 @@
+# エフェクトを操作するモジュール
 module Effects
+  # 各単語を逆順にする効果
+  # 
+  # return [Proc] リバースエフェクト効果
   def self.reverse
     ->(words) do
       words.split(' ').map(&:reverse).join(' ')
     end
   end
 
+  # 指定された回数だけ各文字を繰り返す効果
+  # 
+  # @params rate [Integer] 回数
+  # return [Proc] エコーエフェクト効果
   def self.echo(rate)
     ->(words) do
       words.chars.map{ |c| c == ' ' ? c : c * rate }.join
     end
   end
 
+  # 全て大文字に変換し、各単語の末尾に指定された回数"!"をつける効果
+  # 
+  # @params level [Integer] 回数
+  # return [Proc] ラウドエフェクト効果
   def self.loud(level)
     ->(words) do
       words.split(' ').map{ |word| word.upcase + '!' * level }.join(' ')
--- chapter10/lib/word_synth.rb
+++ chapter10/lib/word_synth.rb
@@ -1,12 +1,21 @@
+# ワードシンセサイザーを操作するクラス
 class WordSynth
+  # エフェクト配列を初期化する
   def initialize
     @effects = []
   end
 
+  # エフェクト配列に新たなエフェクト効果を追加する
+  # 
+  # @params effect [Proc] エフェクト効果
   def add_effect(effect)
     @effects << effect
   end
 
+  # 入力された文字列にエフェクト配列の効果を加えて出力する
+  # 
+  # @params original_words [String] 文字列
+  # return [String] エフェクト配列の効果を加えた文字列
   def play(original_words)
     @effects.inject(original_words) do |word, effect|
       effect.call(word)
